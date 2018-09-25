b0514b8f0ac0567cdecba97add27a3f012f919a9
upalr <shovon.forums@gmail.com>
2018-09-25 12:28:22 +1000

    commit changes bug login failed

--- CabsOnline/settings.php
+++ CabsOnline/settings.php
@@ -1,6 +1,6 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pwd  = "1234";
-	$sql_db  = "s101918586_db";
+    $host = "feenix-mariadb.swin.edu.au";
+    $user = "s101918586";
+    $pwd = "140992";
+    $sql_db = "s101918586_db";
 ?>
