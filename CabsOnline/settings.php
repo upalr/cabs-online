b572034ccb5e7ee3b8496d7f2570d8bc1d2c2df7
upalr <shovon.forums@gmail.com>
2018-09-25 12:43:12 +1000

    commit DB connection issue

--- CabsOnline/settings.php
+++ CabsOnline/settings.php
@@ -1,6 +1,6 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pwd = "1234";
+    $host = "feenix-mariadb.swin.edu.au";
+    $user = "s101918586";
+    $pwd = "140992";
     $sql_db = "s101918586_db";
 ?>
